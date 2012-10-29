040c2af08b43ec3254064df2c0a0f6664ad6e5af
charles.heath.berman@gmail.com <charles.heath.berman@gmail.com@b97491a3-796a-49ba-9b87-07b6f181f6e0>
2012-10-29 16:36:51 +0000

    updated

--- src/Interfaces/IDocumentLine.cs
+++ src/Interfaces/IDocumentLine.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
-// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
-
+﻿
 namespace miRobotEditor.Interfaces
 {
     /// <summary>
