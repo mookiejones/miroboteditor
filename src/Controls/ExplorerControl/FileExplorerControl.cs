acf67f8784fae5d75540b64e24e30303e9562d7f
charles.heath.berman@gmail.com <charles.heath.berman@gmail.com@b97491a3-796a-49ba-9b87-07b6f181f6e0>
2012-10-30 16:55:27 +0000

    Added File Explorer.
    Needs to be adapted for different robots plug ContextMenuItems Need to Be Added

---
+++ src/Controls/ExplorerControl/FileExplorerControl.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cberman
+ * Date: 10/30/2012
+ * Time: 12:37
+ * 
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace miRobotEditor.Controls.ExplorerControl
+{
+	/// <summary>
+	/// Description of FileExplorerControl.
+	/// </summary>
+	public partial class FileExplorerControl : UserControl
+	{
+		public static FileExplorerControl Instance{get;set;}
+		public String Filter{get {return cmbFilter.SelectedText;}}
+		public FileExplorerControl()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			Instance = this;
+			
+
+		}
+	}
+}
