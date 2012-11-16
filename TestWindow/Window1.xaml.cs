7ffd3c2367bbc77e0406a6cf4cade5329432add7
charles.heath.berman@gmail.com <charles.heath.berman@gmail.com@b97491a3-796a-49ba-9b87-07b6f181f6e0>
2012-11-16 12:04:19 +0000

    Add Test Window
    Creating Framework for FTP

---
+++ TestWindow/Window1.xaml.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cberman
+ * Date: 11/16/2012
+ * Time: 07:00
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TestWindow
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/GUI/Options/Options.xaml.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cberman
+ * Date: 11/14/2012
+ * Time: 3:06 PM
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace miRobotEditor.GUI.Options
+{
+	/// <summary>
+	/// Interaction logic for Options.xaml
+	/// </summary>
+	public partial class Options : Window
+	{
+		public Options()
+		{
+			InitializeComponent();
+		}
+	}
+}
